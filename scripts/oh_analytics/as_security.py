f7e4b800a3b593ec9588758312dd57323bef9350
Adam Przybilla <adprzy@microsoft.com>
2021-06-21 21:03:57 -0400

    Add garage door

--- scripts/oh_analytics/as_security.py
+++ scripts/oh_analytics/as_security.py
@@ -5,28 +5,35 @@ import math
 from datetime import datetime, date, time
 import logging
 import sys
-import oh_sql_client
 from os.path import exists
+import time
 
 # This analytics script covers several areas:
-#   Garage Door
+#   Garage Door - create a timer that expires at a specific time (e.g. 9:00pm) and checks garage door status
 #   Open door for extended period of time
 #   Door or Motion detected while away
+
+class garage_door():
+    _garage_door_down_sensor = "MoxaMR451600DI_ExternalGarageDoorDownSensor"
+    _check_closed_time = time.time()
+    def __init__(self, logger):
+        
 
 class as_security(): 
     # Globals
     _logger = None
     _analytics_conf = dict()
-    _garage_door_down_sensor = "MoxaMR451600DI_ExternalGarageDoorDownSensor"
+    # Garage Door
+    
+    _security_msg_uid = "security_analytics_msg"
+    
     _oh_client = None
 
     # Init 
     def __init__(self, logger) -> None:
         self._logger = logger
         self._logger.info('Security Analytics object init')
-        self._oh_client = oh_sql_client.oh_sql_client(None)
         self._init_analytics_conf()
-        self._backlog_data()
         pass
                 
     # Open Hab Globals
@@ -35,9 +42,9 @@ class as_security():
         json_file_name = 'oh_analytics_conf.json'
 
         # Create conf entries
-        self._analytics_conf['oh_token'] = 'SQL User Name'
-        self._analytics_conf['host'] = 'OH SQL Host Name'
-        self._analytics_conf['port'] = '3306'
+        self._analytics_conf['oh_token'] = 'OH_Token'
+        self._analytics_conf['host'] = 'debian-openhab'
+        self._analytics_conf['port'] = '8080'
 
         # Read json conf
         try:
@@ -90,62 +97,36 @@ class as_security():
                     self.report_to_OH()
                     self.logger.info(f'Weather Station Temperture: {value} deg F')
 
-    def analyze_data(self, new_value):
-        self._logger.info(f"Analyzing Data: {new_value}")
-        if self._last_timestamp is None:
-            self._last_timestamp  = datetime.now()
-
-        # Check if min/max values should be reset
-        now = datetime.now()
-        # Reset Day
-        if now.day != self._last_timestamp.day:
-            self.MaxTempToday = -math.inf
-            self.MinTempToday = math.inf
-            self._logger.info(f'Reseting Day: {now} DNE {self._last_timestamp}')
-        # Reset Month
-        if now.month != self._last_timestamp.month:
-            self.MaxTempMonth = -math.inf
-            self.MinTempMonth = math.inf
-            self._logger.info(f'Reseting Month: {now} DNE {self._last_timestamp}')
-        self._last_timestamp = now
-        # Update Min/max values
-        self.MaxTempToday = max(self.MaxTempToday, new_value)
-        self.MinTempToday = min(self.MinTempToday, new_value)
-        self.MaxTempMonth = max(self.MaxTempMonth, new_value)
-        self.MinTempMonth = min(self.MinTempMonth, new_value)
-
-    def report_to_OH(self):
-        self._logger.info("Reporting to Open Hab")
-        daily_max_item_uid = "WS_Temperature_DailyMax"
-        #     "http://debian-openhab:8080/rest/items/WS_Temperature_DailyMax/state"
-        url = f"http://{self._analytics_conf['host']}:{self._analytics_conf['port']}/rest/items/{daily_max_item_uid}/state"
-        request = requests.put(url,  auth=(self._analytics_conf['oh_token'], ''), data=str(self.MaxTempToday), headers={ "Content-Type": "text/plain" })
-        self._logger.info(f"MaxTempToday:Request:put:{request.status_code}")
-        self._logger.info(f"MaxTempToday:{self.MaxTempToday}")
-        daily_min_item_uid = "WS_Temperature_DailyMin"
-        url = f"http://{self._analytics_conf['host']}:{self._analytics_conf['port']}/rest/items/{daily_min_item_uid}/state"
-        request = requests.put(url,  auth=(self._analytics_conf['oh_token'], ''), data=str(self.MinTempToday), headers={ "Content-Type": "text/plain" })
-        self._logger.info(f"MinTempToday:Request:put:{request.status_code}")
-        self._logger.info(f"MinTempToday:{self.MinTempToday}")
+    def report_to_OH(self, message):
+        self._logger.info("Reporting security message to Open Hab")
+        url = f"http://{self._analytics_conf['host']}:{self._analytics_conf['port']}/rest/items/{_security_msg_uid}/state"
+        request = requests.put(url,  auth=(self._analytics_conf['oh_token'], ''), data=str(message), headers={ "Content-Type": "text/plain" })
+        self._logger.info(f"security_analytics_msg:Request:put:{request.status_code}")
+        self._logger.info(f"security_analytics_msg:{message}")
 
 def main():
     # Configure Logger
     logging.basicConfig(level=logging.INFO, format="%(asctime)s:%(levelname)s:%(message)s")
     logger = logging.getLogger(__name__)
     # Debug File Log
-    file = logging.FileHandler("debug_as_temperature.log")
+    file = logging.FileHandler("debug_as_security.log")
     file.setLevel(logging.INFO)
     fileformat = logging.Formatter("%(asctime)s:%(levelname)s:%(message)s")
     file.setFormatter(fileformat)
     logger.addHandler(file)
     # Critical File Log
-    cric_file = logging.FileHandler("critical_as_temperature.log")
+    cric_file = logging.FileHandler("critical_as_security.log")
     cric_file.setLevel(logging.CRITICAL)
     cric_file.setFormatter(fileformat)
     logger.addHandler(cric_file)
     # Create Temperature Analytics obj and wait for OH item status change
     ast = as_security(logger)
-    ast.wait_for_item_status_change()
+    ast.report_to_OH("")
+    time.sleep(2)
+    ast.report_to_OH("Create Temperature Analytics obj and wait for OH item status change")
+    time.sleep(2)
+    ast.report_to_OH("")
+    
 
 if __name__ == "__main__":
     main()
